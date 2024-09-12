06dc828d6781edc2b19cb38760f47c7d726c0186
patricia.siqueira <patriciafs1@gmail.com>
2024-09-12 16:15:42 -0300

    Patricia

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -2,4 +2,3 @@ it('testa a página da política de privacidade de forma independente', function
     cy.visit('./src/privacy.html')
     cy.contains('Talking About Testing').should('be.visible')
 })
-.
